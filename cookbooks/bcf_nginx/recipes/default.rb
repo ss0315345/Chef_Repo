7ceaf732bb40e7a8133ae48f30de75ed79004414
Sagar Sharma <s_agarsharma@hotmail.com>
2020-03-23 16:30:20 -0400

    5th commit

--- cookbooks/bcf_nginx/recipes/default.rb
+++ cookbooks/bcf_nginx/recipes/default.rb
@@ -3,4 +3,10 @@ package "nginx"
 service "nginx" do
 	action [:enable, :start]
 end
+
+file "/usr/share/nginx/html/index.html" do
+	content "<html><h1>Hello World</h1></html>"
+	action :create
+	not_if { ::File.exist?("/usr/share/nginx/html/index.html") }
+end
 	
